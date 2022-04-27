02b0f1ec5dcfd3fdf8ccdb299065bca0045092a7
Grant <hgranthorner@gmail.com>
2022-04-26 22:07:36 -0400

    initial commit

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((cider-shadow-default-options . "frontend")
+     (cider-default-cljs-repl . shadow)
+     (cider-lein-parameters . "with-profile +dev repl :headless :host localhost")
+     (cider-clojure-cli-global-options . "-A:dev"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
